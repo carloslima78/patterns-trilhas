cb733d4063c23c6f8c2d43a4bd57e5dc70c4a4b8
carloslima78 <carlos.lima78@gmail.com>
2024-01-10 17:28:52 -0300

    app spring boot

--- circuit-breaker/java-springboot/app/crm/src/main/java/com/resilience/crm/controller/CrmController.java
+++ circuit-breaker/java-springboot/app/crm/src/main/java/com/resilience/crm/controller/CrmController.java
@@ -1,6 +1,8 @@
 package com.resilience.crm.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/crm")
@@ -15,25 +18,65 @@ public class CrmController {
 
     private RestTemplate restTemplate;
 
-    @GetMapping
-    @CircuitBreaker(name = "crm", fallbackMethod = "crmFallback")
-    public List<Map<String, Object>> crm(){
+    @GetMapping("/circuitbreaker")
+    @CircuitBreaker(name = "crm", fallbackMethod = "ciruitBreakerFallback")
+    public String getClientCircuitBreaker(){
         restTemplate = new RestTemplate();
+
         // Faz a requisição ao serviço cliente para obter a lista de clientes
         List<Map<String, Object>> clients = restTemplate.getForObject("http://localhost:8081/client", List.class);
 
-        // Adiciona uma mensagem específica do serviço CRM
-        String crmMessage = "Oferta especial de 10% de desconto em todos os produtos!";
+        // Adiciona uma mensagem específica do serviço CRM a cada cliente e concatena as mensagens
+        String crmMessage = clients.stream()
+                .map(client -> "Proposta enviado ao cliente " + client.get("nome") + " no email " + client.get("email"))
+                .collect(Collectors.joining("\n"));
 
-        // Adiciona a mensagem à lista de clientes
-        for (Map<String, Object> client : clients) {
-            client.put("Mensagem", crmMessage);
-        }
-
-        return clients;
+        return crmMessage;
     }
 
-    public String crmFallback(Exception e){
-        return "Fallback para o serviço CRM";
+    int count = 1;
+
+    @GetMapping("/retry")
+    @Retry(name = "crm")
+    public String getClientRetry(){
+
+        System.out.println("Foram " + count++ + " tentativas de requisições ao serviço de clientes");
+
+        restTemplate = new RestTemplate();
+
+        // Faz a requisição ao serviço cliente para obter a lista de clientes
+        List<Map<String, Object>> clients = restTemplate.getForObject("http://localhost:8081/client", List.class);
+
+        // Adiciona uma mensagem específica do serviço CRM a cada cliente e concatena as mensagens
+        String crmMessage = clients.stream()
+                .map(client -> "Proposta enviado ao cliente " + client.get("nome") + " no email " + client.get("email"))
+                .collect(Collectors.joining("\n"));
+
+        return crmMessage;
+    }
+
+    @GetMapping("/ratelimiter")
+    @RateLimiter(name = "crm", fallbackMethod = "reteLimiterBreakerFallback")
+    public String getClientRateLimiter(){
+
+        restTemplate = new RestTemplate();
+
+        // Faz a requisição ao serviço cliente para obter a lista de clientes
+        List<Map<String, Object>> clients = restTemplate.getForObject("http://localhost:8081/client", List.class);
+
+        // Adiciona uma mensagem específica do serviço CRM a cada cliente e concatena as mensagens
+        String crmMessage = clients.stream()
+                .map(client -> "Proposta enviado ao cliente " + client.get("nome") + " no email " + client.get("email"))
+                .collect(Collectors.joining("\n"));
+
+        return crmMessage;
+    }
+
+    public String ciruitBreakerFallback(Exception e){
+        return "Fallback: Serviço de clientes está fora do ar";
+    }
+
+    public String reteLimiterBreakerFallback(Exception e){
+        return "Fallback: Estourou a quantidade de chamadas";
     }
 }
